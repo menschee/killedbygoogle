eff12d202109c7e9186a241b384cdd1d995e2292
Daniil <104896330+menschee@users.noreply.github.com>
2025-11-26 11:44:39 +0300

    Update index.tsx

--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,9 @@
 export {Controls} from './Controls';
-export {default as Carbon} from './Carbon';
+// export {default as Carbon} from './Carbon'; // remove ads
 export {default as Filter} from './Filter';
 export {default as Loader} from './Loader';
 export {default as SRT} from './SRT';
-// export {default as PressCoverage} from './PressCoverage';
+// export {default as PressCoverage} from './PressCoverage'; // remove press stuff
 export {default as List} from './List';
 export {default as Footer} from './Footer';
 export {default as Header} from './Header';
